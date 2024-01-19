6b105f1b4909978c49afed9d2714d8ed51bdf45a
AZTSA-3349 <anthonyzh3349@gmail.com>
2024-01-19 13:10:10 -0500

    Create Lift.java

---
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Lift extends SubsystemBase {
+    private Victor leftMotor;
+    private Victor rightMotor;
+    
+    private final double LIFT_SPEED = .5;
+
+    public Lift(int leftChannel, int rightChannel) {
+        leftMotor = new Victor(leftChannel);
+        rightMotor = new Victor(rightChannel);
+    }
+
+    public void extend() {
+        leftMotor.set(LIFT_SPEED);
+        rightMotor.set(LIFT_SPEED);
+    } 
+
+    public void retract() {
+        leftMotor.set(-LIFT_SPEED);
+        rightMotor.set(-LIFT_SPEED);
+    }
+
+    public void stop() {
+        leftMotor.set(0);
+        rightMotor.set(0);
+    }
+}
